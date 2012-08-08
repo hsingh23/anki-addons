caf591e6ffdc6bb683987bb56a7a7743b9194832
ospalh <ospalh@gmail.com>
2012-08-08 11:22:52 +0200

    Actually show a tip.

---
+++ scripts/dvorak_tips.js
@@ -0,0 +1,20 @@
+
+
+
+
+$(document).ready(function(){
+
+    $('#pinky').qtip({
+        content: {
+            text:'The semicolon “;” work as easy, as well, \
+like this, it should work for the programmer dvorak variant as well.',
+        },
+        style: {
+            classes: 'ui-tooltip-rounded ui-tooltip-shadow',
+        },
+        show: {
+	    solo: true
+	}
+    });
+
+});
